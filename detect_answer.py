a3740d911f28892faff3ff3567bc6142a5994b44
Johan Liebert <johan.liebert@gmail.com>
2025-05-15 00:58:14 +0800

    Predefined constants for image enhancer

--- detect_answer.py
+++ detect_answer.py
@@ -1,8 +1,10 @@
+from enhance_image import image_enhancer
 from grade_paper import ProcessPage
 from transform_image import transform_paper_image
 
 
 def answer_detector(image):
+    image = image_enhancer(image, block_size=51, blur_ksize=5, C=9, morph_kernel_size=1)
     image, paper, biggestContour = transform_paper_image(image)
     answers, paper, codes = ProcessPage(paper)
     return image, paper, biggestContour, answers, codes
